2df95b359ebf2f8b1e658785e696f00e7e96b571
CamD67 <cameron.doane@gmail.com>
2017-03-07 16:16:56 -0800

    remove unnecessary imports

--- StreamSurfer/app/src/main/java/com/streamsurfer/surfers/streamsurfer/BaseActivity.java
+++ StreamSurfer/app/src/main/java/com/streamsurfer/surfers/streamsurfer/BaseActivity.java
@@ -1,22 +1,15 @@
 package com.streamsurfer.surfers.streamsurfer;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
-import android.widget.ListView;
 import android.widget.Toast;
 
 public class BaseActivity extends AppCompatActivity {
