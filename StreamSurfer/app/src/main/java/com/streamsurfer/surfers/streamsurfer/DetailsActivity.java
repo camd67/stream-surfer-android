65e7dab8e04a813f6d756dd1b61ea3d155ab163f
CamD67 <cameron.doane@gmail.com>
2017-03-08 02:21:09 -0800

    fix merge conflict bugs

--- StreamSurfer/app/src/main/java/com/streamsurfer/surfers/streamsurfer/DetailsActivity.java
+++ StreamSurfer/app/src/main/java/com/streamsurfer/surfers/streamsurfer/DetailsActivity.java
@@ -4,7 +4,7 @@ package com.streamsurfer.surfers.streamsurfer;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View
+import android.view.View;
 import android.widget.Button;
 import android.widget.AdapterView;
 import android.widget.ListView;
